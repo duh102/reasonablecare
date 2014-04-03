615ca4cf1a950446b672cf449baf0f3c2f744231
Will <wemorrow@ncsu>
2014-04-03 14:01:35 -0400

    added dbminder

--- src/reasonablecare/ReasonableCare.java
+++ src/reasonablecare/ReasonableCare.java
@@ -1,6 +1,9 @@
 package reasonablecare;
 
 import java.util.*;
+import java.sql.*;
+
+import reasonablecare.data.*;
 
 public class ReasonableCare
 {
@@ -8,6 +11,9 @@ public class ReasonableCare
   {
     Scanner scan = new Scanner(System.in);
     scan.useDelimiter("\\n");
+    
+    boolean loggedIn = false;
+    
     System.out.println("Welcome to ReasonableCare's client! Please log in below.");
     System.out.print("User types:\n[0] Student\n[1] Nurse\n[2] Doctor\n> ");
     int type = scan.nextInt();
@@ -19,16 +25,37 @@ public class ReasonableCare
     {
       case 0:
         System.out.printf("\nYou're attempting to log in as a student with UID [%d] and password '%s'\n", uid, pass);
+        loggedIn = logIn(type, uid, pass);
         break;
       case 1:
         System.out.printf("\nYou're attempting to log in as a nurse with UID [%d] and password '%s'\n", uid, pass);
+        loggedIn = logIn(type, uid, pass);
         break;
       case 2:
         System.out.printf("\nYou're attempting to log in as a doctor with UID [%d] and password '%s'\n", uid, pass);
+        loggedIn = logIn(type, uid, pass);
         break;
       default:
-        System.out.printf("\nYou're attempting to log in as a [%d] with UID [%d] and password '%s'\n", type, uid, pass);
+        System.out.printf("\nYou're attempting to log in as a [%d] with UID [%d] and password '%s'\nYou can't do that, stop.\n", type, uid, pass);
         break;
     }
+    if(loggedIn)
+    {
+    }
+    else
+    {
+    }
+    System.out.println("Goodbye!");
+  }
+  
+  
+  public static boolean logIn(int type, int uid, String password)
+  {
+    DBMinder minder = DBMinder.instance();
+    Connection conn = minder.getConnection();
+    switch(type)
+    {
+    }
+    return false;
   }
 }
---
+++ src/reasonablecare/data/DBMinder.java
@@ -0,0 +1,79 @@
+package reasonablecare.data;
+
+import java.sql.*;
+
+public class DBMinder
+{
+  private static final String jdbcURL = "jdbc:oracle:thin:@ora.csc.ncsu.edu:1521:orcl";
+  private static final String user = "wemorrow";
+  private static final String password = "trees";
+  
+  private static DBMinder minderSingleton = null;
+  
+  private Connection conn = null;
+  
+  private DBMinder()
+  {
+    getConnection();
+  }
+  
+  public static DBMinder instance()
+  {
+    if(minderSingleton == null)
+    {
+      minderSingleton = new DBMinder();
+    }
+    return minderSingleton;
+  }
+  
+  
+  /* getConnection() returns either a Connection object or null
+   * Connection if the connection was successful
+   * null if the connection was not successful
+   */
+  public Connection getConnection()
+  {
+    if(conn == null)
+    {
+      try
+      {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        conn = DriverManager.getConnection(jdbcURL, user, password);
+      }
+      catch (ClassNotFoundException e)
+      {
+        e.printStackTrace();
+      }
+      catch (SQLException e)
+      {
+        e.printStackTrace();
+      }
+      finally
+      {
+        return conn;
+      }
+    }
+    else
+    {
+      return conn;
+    }
+  }
+  
+  /* close() closes the connection if it's still connected
+   * not a whole lot to this guy
+   */
+  private void close()
+  {
+    if(conn != null)
+    {
+      try
+      {
+        conn.close();
+      }
+      catch (SQLException e)
+      {
+        e.printStackTrace();
+      }
+    }
+  }
+}
