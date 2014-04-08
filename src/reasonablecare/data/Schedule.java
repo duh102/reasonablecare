c72a5a2961214a25f7d82b0e82d18069814b8730
Will <duh102@gmail.com>
2014-04-08 13:55:48 -0400

    added comments; added modifyUser function

--- src/reasonablecare/data/Schedule.java
+++ src/reasonablecare/data/Schedule.java
@@ -6,6 +6,8 @@ import java.sql.*;
 
 public class Schedule
 {
+  /* returns the free slots during the day that a given doctor has on a given day
+   */
   public static List<Integer> retrieveDoctorFreeSlots(Date date, int doctorID)
   {
     DBMinder minder = DBMinder.instance();
@@ -16,16 +18,46 @@ public class Schedule
     try
     {
       //retrieve hours working
-      //retrieve appointments that date
-      ps = conn.prepareStatement("select time_slot_beginning, time_slot_end from appointment where doctor_id = ? and apt_date = ?;");
+      int timeBegin = 0, timeEnd = 0;
+      ps = conn.prepareStatement("SELECT start_timeslot, end_timeslot FROM WorkDay WHERE doctor_id = ? AND day_of_week = (SELECT D - 1 FROM(SELECT TO_CHAR (DATE ?, 'D') D FROM DUAL))");
       ps.setInt(1, doctorID);
       ps.setDate(2, date);
       rs = ps.executeQuery();
-      //expand hours working and do set difference
+      while(rs.next())
+      {
+        timeBegin = rs.getInt(1);
+        timeEnd = rs.getInt(2);
+      }
+      rs.close();
+      
+      //expand hours working
+      for(int i = timeBegin; i <= timeEnd; i++)
+      {
+        toReturn.add(new Integer(i));
+      }
+      
+      //retrieve appointments that date
+      ps = conn.prepareStatement("select time_slot_beginning, time_slot_end from appointment where doctor_id = ? and apt_date = ?");
+      ps.setInt(1, doctorID);
+      ps.setDate(2, date);
+      rs = ps.executeQuery();
+      
+      //remove occupied time slots
+      while(rs.next())
+      {
+        timeBegin = rs.getInt(1);
+        timeEnd = rs.getInt(2);
+        for(int i = timeBegin; i <= timeEnd; i++)
+        {
+          toReturn.remove(new Integer(i));
+        }
+      }
+      rs.close();
       //return list
     }
     catch(SQLException sqle)
     {
+      sqle.printStackTrace();
     }
     return toReturn;
   }
--- src/reasonablecare/data/User.java
+++ src/reasonablecare/data/User.java
@@ -4,6 +4,10 @@ import java.sql.*;
 
 public class User
 {
+  /* returns the true if a given user of given type and given password match what is in the database
+   * this is not very secure and I would never use this in production code but it works
+   * for the project's requirements
+   */
   public static boolean logInUser(int type, int uid, String password)
   {
     DBMinder minder = DBMinder.instance();
@@ -36,7 +40,66 @@ public class User
     }
     catch(SQLException sqle)
     {
+      sqle.printStackTrace();
     }
     return returnedID == uid;
+  }
+  
+  /* returns true if an update on a user processed successfully
+   * newName and newPass can be empty, in which case nothing happens
+   * if either or both contain new values, the user is updated with those new values
+   */
+  public static boolean modifyUser(int type, int uid, String newName, String newPass)
+  {
+    DBMinder minder = DBMinder.instance();
+    Connection conn = minder.getConnection();
+    String personType = "";
+    int neededModifications = 0, doneModifications = 0;;
+    switch(type)
+    {
+      case 0:
+        personType = "Student";
+        break;
+      case 1:
+        personType = "Nurse";
+        break;
+      case 2:
+        personType = "Doctor";
+        break;
+    }
+    try
+    {
+      if(newName.length() > 0)
+      {
+        neededModifications++;
+        PreparedStatement ps = conn.prepareStatement("UPDATE "+personType+" SET Name = ? WHERE id = ?");
+        ps.setString(1, newName);
+        ps.setInt(2, uid);
+        int numRowsAffected = ps.executeUpdate();
+        doneModifications+= numRowsAffected;
+      }
+      if(newPass.length() > 0)
+      {
+        PreparedStatement ps = conn.prepareStatement("UPDATE "+personType+" SET Login_Info = ? WHERE id = ?");
+        ps.setString(1, newPass);
+        ps.setInt(2, uid);
+        int numRowsAffected = ps.executeUpdate();
+        doneModifications+= numRowsAffected;
+      }
+      //each update really shouldn't affect more than one row, that'd be really weird
+      if(doneModifications >= neededModifications)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    catch(SQLException sqle)
+    {
+      sqle.printStackTrace();
+    }
+    return false;
   }
 }
